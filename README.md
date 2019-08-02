8330a1664edbad136c7bc7a127e74f581b218bdc
Anna Bagrowska <anna_bagrowska@wp.pl>
2019-08-02 20:51:35 +0200

    change addBtn & add resetBtn

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Task with JavaScript
 
-In this task I created three buttons:
+In this task I created four buttons:
 
-* button, which add ten element list
+* button, which add element list
 * button, which increase fontsize by 1px
 * button, which decrese fontsize by 1px.
+* button, which reset changes (created in JS)
 
-I  used forEach function.
+I  used forEach function to increase and decrease fontsize.
 
 https://annbag.github.io/JS-task-1/
--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 const btn = document.querySelector('button.btn');
 const increaseBtn = document.querySelector('button.increase-btn');
 const decreaseBtn = document.querySelector('button.decrease-btn');
-const elements = document.querySelectorAll('li');
 
 let fontSize = 16;
+let order = 1;
+
+const resetBtn = document.createElement('button');
+document.body.appendChild(resetBtn);
+resetBtn.classList.add('btn');
+resetBtn.textContent = 'Reset';
+
+const list = document.createElement('ul');
+document.body.appendChild(list);
 
 btn.addEventListener('click', () => {
-  elements.forEach(element => {
-    element.style.display = 'block';
-  });
+  const li = document.createElement('li');
+  li.textContent = `Element ${order++}`;
+  list.appendChild(li);
+  li.style.fontSize = fontSize + 'px';
 });
 
 increaseBtn.addEventListener('click', () => {
+  const elements = document.querySelectorAll('li');
   fontSize += 1;
   elements.forEach(element => {
-    element.style.fontSize = fontSize + 'px';
+    element.style.fontSize = `${fontSize} px`;
   });
 });
 
 decreaseBtn.addEventListener('click', () => {
+  const elements = document.querySelectorAll('li');
   fontSize -= 1;
   elements.forEach(element => {
     element.style.fontSize = fontSize + 'px';
   });
 });
+
+resetBtn.addEventListener('click', () => {
+  list.textContent = '';
+  fontSize = 16;
+  order = 1;
+});
+
+
